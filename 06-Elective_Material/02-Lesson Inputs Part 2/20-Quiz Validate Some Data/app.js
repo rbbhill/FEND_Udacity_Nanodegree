7027223e9d707ae67f6cd54037a2a5c7680d77a5
Rick Barnhill <rbbhill@gmail.com>
2018-05-07 14:58:10 -0500

    Module 6 Completion Update
    Extracurricular Activities in module 6 completed and submitted.

--- 06-Elective_Material/02-Lesson Inputs Part 2/20-Quiz Validate Some Data/app.js
+++ 06-Elective_Material/02-Lesson Inputs Part 2/20-Quiz Validate Some Data/app.js
@@ -2,9 +2,6 @@ var firstPasswordInput = document.querySelector('#first');
 var secondPasswordInput = document.querySelector('#second');
 var submit = document.querySelector('#submit');
 
-
-
-
 function IssueTracker() {
   this.issues = [];
 }
@@ -16,144 +13,69 @@ IssueTracker.prototype = {
     var message = "";
     switch (this.issues.length) {
       case 0:
-
         break;
       case 1:
-        message = "Please correct the following issue:\n" + this.issue[0];
+        message = "Please correct the following issue:\n" + this.issues[0];
         break;
       default:
-        message = "Please correct the following issues:\n" + this.issues.join(\n);
+        message = "Please correct the following issues:\n" + this.issues.join("\n");
         break;
     }
     return message;
   }
 };
 
-
-
-
-
-
-
-submit.onclick = function () {};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-Your code goes here!
- */
-
-/*
-You might find you want to use RegEx. As this quiz is about setCustomValidity
-and not RegEx, here are some RegEx patterns you might find useful:
-
-match one of the required symbols: /[\!\@\#\$\%\^\&\*]/g
-match a number: /[0-9]/g or /\d/g
-match a lowercase letter: /[a-z]/g
-match an uppercase letter: /[A-Z]/g
-match a character that isn't allowed in this password: /[^A-z0-9\!\@\#\$\%\^\&\*]/g
- */
-
-/*
-Grabbing a few inputs to help you get started...
- */
+submit.onclick = function () {
+  var firstPassword = firstPasswordInput.value;
+  var secondPassword = secondPasswordInput.value;
+  var firstInputIssuesTracker = new IssueTracker();
+  var secondInputIssuesTracker = new IssueTracker();
+
+  function checkRequirements() {
+    if (firstPassword.length < 16) {
+      firstInputIssuesTracker.add("fewer than 16 characters");
+    } else if (firstPassword.length > 100) {
+      firstInputIssuesTracker.add("greater than 100 characters");
+    }
+
+    if (!firstPassword.match(/[\!\@\#\$\%\^\&\*]/g)) {
+      firstInputIssuesTracker.add("missing a symbol (!, @, #, $, %, ^, &, *)");
+    }
+
+    if (!firstPassword.match(/\d/g)) {
+      firstInputIssuesTracker.add("missing a number");
+    }
+
+    if (!firstPassword.match(/[a-z]/g)) {
+      firstInputIssuesTracker.add("missing a lowercase letter");
+    }
+
+    if (!firstPassword.match(/[A-Z]/g)) {
+      firstInputIssuesTracker.add("missing an uppercase letter");
+    }
+
+    var illegalCharacterGroup = firstPassword.match(/[^A-z0-9\!\@\#\$\%\^\&\*]/g)
+    if (illegalCharacterGroup) {
+      illegalCharacterGroup.forEach(function (illegalChar) {
+        firstInputIssuesTracker.add("includes illegal character: " + illegalChar);
+      });
+    }
+  };
+
+  if (firstPassword === secondPassword && firstPassword.length > 0) {
+
+      checkRequirements();
+  } else {
+    secondInputIssuesTracker.add("Passwords must match!");
+  }
+
+  var firstInputIssues = firstInputIssuesTracker.retrieve()
+  var secondInputIssues = secondInputIssuesTracker.retrieve()
+
+  firstPasswordInput.setCustomValidity(firstInputIssues);
+  secondPasswordInput.setCustomValidity(secondInputIssues);
+
+  if (firstInputIssues.length + secondInputIssues.length === 0) {
+    alert("Password change successful!");
+  }
+};
---
+++ Practice/Javascript/app.js
@@ -0,0 +1,42 @@
+function foo () {
+  a=a*2;
+  a=a+3;
+}
+
+var a = 10;
+
+foo ();
+
+console.log(a);
+
+foo();
+foo();
+
+console.log(a);
+
+
+function foo (b) {
+  a=a*2;
+  a=a+b;
+}
+var a = 10;
+
+foo(5);
+foo(25);
+foo(100);
+
+console.log(a);
+
+/**/
+function foo (b) {
+  a=a*2;
+  a=a+b;
+  return a/2;
+}
+
+var a = 10;
+var b = foo(3);
+
+console.log(a);
+
+/**/
